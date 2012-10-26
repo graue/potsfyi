d3c58ea8fcd0f7a057521da577cc83f4ff1db91f
Scott Feeney <scott@oceanbase.org>
2012-10-26 01:40:43 -0500

    Add tracks to a simple database
    This uses Flask-Script to run the DB create command,
    Flask-SQLAlchemy as an ORM and Mutagen to read tags.

---
+++ manage.py
@@ -0,0 +1,29 @@
+import os
+import mutagen
+from flask.ext.script import Manager
+from potsfyi import db, Track, app
+
+manager = Manager(app)
+
+
+@manager.command
+def createdb():
+    db.create_all()
+    music_dir = app.config['MUSIC_DIR']
+
+    for path, dirs, files in os.walk(music_dir):
+        for file in files:
+            tag_info = mutagen.File(os.path.join(path, file), easy=True)
+            if tag_info is not None:
+                artist = tag_info.tags['artist'][0]
+                title = tag_info.tags['title'][0]
+                filename = os.path.join(path, file)[len(music_dir) + 1:]
+                print "Adding {0} - {1} at {2}".format(artist, title, filename)
+                new_track = Track(artist, title, filename)
+                db.session.add(new_track)
+
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    manager.run()
--- potsfyi.py
+++ potsfyi.py
@@ -1,5 +1,24 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tracks.db'
+app.config['MUSIC_DIR'] = 'static/music'
+db = SQLAlchemy(app)
+
+
+class Track(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist = db.Column(db.String(200))
+    title = db.Column(db.String(240))
+    filename = db.Column(db.String(256))
+
+    def __init__(self, artist, title, filename):
+        self.artist, self.title, self.filename = artist, title, filename
+
+    def __repr__(self):
+        return '<Track {0.artist} - {1.title}>'.format(self)
 
 
 @app.route('/')
