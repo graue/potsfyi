graue/potsfyi

commit 194f0fdf3c2054cde20911689272aa0528274b2b

multiple filename assignments; update manager command doc

manage.py
@@ -15,6 +15,7 @@ HANDLED_FILETYPES = ('.ogg', '.mp3', '.flac', '.m4a')
 
 @manager.command
 def createdb(verbose=False):
+    '''  initial creation of the tracks database '''
     db.create_all()
     music_dir = unicode(app.config['MUSIC_DIR'])
 
@@ -24,7 +25,8 @@ def createdb(verbose=False):
                 continue
 
             filename_with_musicdir = os.path.join(path, file)
-            filename = filename_with_musicdir[len(music_dir) + 1:]
+            filename = os.path.basename(filename_with_musicdir)
+            print( filename )
 
             try:
                 tag_info = mutagen.File(filename_with_musicdir, easy=True)
@@ -35,7 +37,6 @@ def createdb(verbose=False):
                         sys.exc_info()[0]))
                 continue
 
-            filename = os.path.join(path, file)[len(music_dir) + 1:]
             try:
                 artist = tag_info.tags['artist'][0]
                 title = tag_info.tags['title'][0]
@@ -50,6 +51,11 @@ def createdb(verbose=False):
 
     db.session.commit()
 
+@manager.command
+def update(verbose=False):
+    ''' After createdb is run, this allows you to update the db without
+        duplicating tracks already in the db '''
+    pass
 
 if __name__ == "__main__":
     manager.run()

templates/index.html
@@ -2,7 +2,6 @@
   <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="static/bootstrap.css">
     <title>Pots, fyi</title>
   </head>
   <body>

commit e2384a2d5c5f71e66d1c761dc1a39a5e90ab7f9f

Fix db creation: store path
Database creation was broken by 194f0fdf3c2054cde20911689272aa0528274b2b

manage.py
@@ -24,15 +24,16 @@ def createdb(verbose=False):
             if not file.lower().endswith(HANDLED_FILETYPES):
                 continue
 
-            filename_with_musicdir = os.path.join(path, file)
-            filename = os.path.basename(filename_with_musicdir)
+            full_filename = os.path.join(path, file)
+            relative_filename = full_filename[len(music_dir) + 1:]
 
             try:
-                tag_info = mutagen.File(filename_with_musicdir, easy=True)
+                tag_info = mutagen.File(full_filename, easy=True)
                 if tag_info is None:
                     raise Exception('Mutagen could not open file')
             except:
-                print(u'Skipping {0} due to error: {1}'.format(filename,
+                print(u'Skipping {0} due to error: {1}'.format(
+                        relative_filename,
                         sys.exc_info()[0]))
                 continue
 
@@ -41,12 +42,13 @@ def createdb(verbose=False):
                 title = tag_info.tags['title'][0]
             except (KeyError, IndexError):
                 print(u'Skipping {0}: artist or title tag missing!'
-                      .format(filename))
+                      .format(relative_filename))
                 continue
-            new_track = Track(artist, title, filename)
+            new_track = Track(artist, title, relative_filename)
             db.session.add(new_track)
             if verbose:
-                print(u'Added {0}: {1} - {2}'.format(filename, artist, title))
+                print(u'Added {0}: {1} - {2}'.format(relative_filename,
+                                                     artist, title))
 
     db.session.commit()
 