7c38fc0c4c5b9048645004db2aaddaed1bcd44cd
tippenein <tippenein@gmail.com>
2013-05-09 10:27:22 -0500

    add some update tests

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask.ext.testing import TestCase
 import unittest
 from models import db, Track
-from manage import populate_db
+from manage import populate_db, update_db
 
 class MyTest(TestCase):
 
@@ -38,7 +38,6 @@ def create_mock_tracks(tracks, src_track="test/sinewave.mp3"):
         song_tag.save()
 
 
-
 class TagTest(MyTest):
     mock_tracks = {
         'foo.mp3': {'artist': 'Foo', 'title': 'Bar'},
@@ -64,6 +63,46 @@ class TagTest(MyTest):
         for track in self.mock_tracks:
             os.remove("test/" + track)
         super(TagTest, self).tearDown()
+
+
+class UpdateTest(MyTest):
+    mock_tracks = {
+        'foo.mp3': {'artist': 'Foo', 'title': 'Bar'},
+        'second_thing.mp3': {'artist': 'Someone', 'title': 'A song'},
+    }
+
+    added_track = {
+            'another_one.mp3': {'artist': 'Third Artist', 'title': 'Blobs'}
+            }
+
+    def setUp(self):
+        super(UpdateTest, self).setUp()
+        create_mock_tracks(self.mock_tracks)
+
+    def test_added_track_update(self):
+        added_track = self.added_track
+        populate_db('test', False)
+        create_mock_tracks(added_track)
+        update_db('test', False)
+        filename = added_track.keys()[0]
+        found_track = Track.query.filter_by(artist=added_track[filename]['artist'],
+                                         title=added_track[filename]['title'])
+        assert found_track is not None
+
+    def test_remove_track_update(self):
+        # populate first, then alter and update
+        mock_tracks = self.mock_tracks
+        # pop returns a tuple of (filename, file_info_dict)
+        removed_track = mock_tracks.popitem()[0]  # just track name
+        os.remove(os.path.join('test', removed_track))
+        update_db('test', False)
+        tracks_in_db = Track.query.all()
+        assert removed_track not in tracks_in_db
+
+    def tearDown(self):
+        for track in self.mock_tracks:
+            os.remove("test/" + track)
+        super(UpdateTest, self).tearDown()
 
 
 if __name__ == '__main__':
