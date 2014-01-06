229f52378ca28b4f267990a24d3ecdc7a17efe56
Scott Feeney <scott@oceanbase.org>
2014-01-06 04:55:43 -0500

    Remove separate login bundle
    Now bundle.js is served always, even on the login page. They were
    separated before for two reasons:
    
    * optimization
    * workaround for a bug
    
    Who cares about that silly optimization? As for the bug, a better way is
    to simply not initialize the UI if not logged in. This is now done.

--- script/login.js
+++ script/login.js
@@ -1,55 +1,37 @@
-;(function() {
-    // Simulate jQuery's $(document).ready for modern browsers
-    // Inspired by how ZeptoJS does it
-    function documentReady(callback) {
-        if (/complete|loaded|interactive/.test(document.readyState))
-            callback();
-        else
-            document.addEventListener('DOMContentLoaded', callback, false);
-    }
-
-    function ajaxPost(url, data, success, error) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState != 4) return;
-            xhr.onreadystatechange = function() {};
-            if (xhr.status >= 200 && xhr.status < 300)
-                success(xhr.responseText, xhr);
-            else
-                error(xhr);
-        }
-        xhr.open('POST', '/api/login');
-        xhr.setRequestHeader('Content-Type',
-                             'application/x-www-form-urlencoded');
-        xhr.send(data ? data : null);
-    }
-
-    function gotAssertion(assertion) {
-        if (!assertion)
-            return;
-
-        ajaxPost('/api/login', 'assertion=' + encodeURIComponent(assertion),
-                 function() { location.reload(true); },
-                 function(xhr) { alert('Login error: code ' + xhr.status); });
-    }
-
-    function logoutCallback() {
-        ajaxPost('/api/logout', '',
-                 function() { location.reload(true); },
-                 function(xhr) { alert('Logout error: code '
-                                       + xhr.status); });
-    }
-
-    documentReady(function() {
-        var loginEl = document.getElementById('browserid-login'),
-            logoutEl = document.getElementById('browserid-logout');
-        if (loginEl) loginEl.onclick = function() {
-            navigator.id.get(gotAssertion);
-            return false;
-        };
-        if (logoutEl) logoutEl.onclick = function() {
-            navigator.id.logout(logoutCallback);
-            return false;
-        };
+function ajaxPost(url, data, success, error) {
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        success: success,
+        error: error
     });
-})();
+}
+
+exports.gotAssertion = function(assertion) {
+    if (!assertion)
+        return;
+
+    ajaxPost('/api/login', 'assertion=' + encodeURIComponent(assertion),
+                function() { location.reload(true); },
+                function(xhr) { alert('Login error: code ' + xhr.status); });
+}
+
+exports.logoutCallback = function() {
+    ajaxPost('/api/logout', '',
+                function() { location.reload(true); },
+                function(xhr) { alert('Logout error: code '
+                                    + xhr.status); });
+}
+
+exports.attachAuthHandlers = function() {
+    $('#browserid-login').on('click', function(event) {
+        event.preventDefault();
+        navigator.id.get(exports.gotAssertion);
+    });
+
+    $('#browserid-logout').on('click', function(event) {
+        event.preventDefault();
+        navigator.id.logout(exports.logoutCallback);
+    });
+};
--- script/main.js
+++ script/main.js
@@ -3,7 +3,13 @@
 var $ = require('./lib/jquery.shim');
 require('backbone').$ = $;
 
-$(document).ready(function() {
+$(function() {
+    if (window.loginEnabled)
+        require('./login').attachAuthHandlers();
+
+    if (!window.loggedIn)
+        return;
+
     var PlayingSongInfo = require('./models/PlayingSongInfo'),
         Playlist = require('./models/Playlist'),
         SearchResultList = require('./models/SearchResultList'),
@@ -12,15 +18,15 @@ $(document).ready(function() {
         PlaylistView = require('./views/PlaylistView');
 
     window.playingSong = new PlayingSongInfo();
-    window.playlist = new Playlist(playingSong);
+    window.playlist = new Playlist(window.playingSong);
 
     var resultList = new SearchResultList();
     var resultListView = new SearchResultListView({
         collection: resultList
     });
 
-    resultListView.on('song-clicked', playlist.addSong);
-    resultListView.on('album-clicked', playlist.addAlbum);
+    resultListView.on('song-clicked', window.playlist.addSong);
+    resultListView.on('album-clicked', window.playlist.addAlbum);
 
     $('input#search-box').on('keyup', function() {
         var newValue = $('input#search-box').val();
