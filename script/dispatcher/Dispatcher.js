8bfef3a7d55efd924e50238e505b2d94f682db81
Scott Feeney <scott@oceanbase.org>
2015-07-04 03:01:33 -0700

    ES2015-ify Dispatcher

--- script/dispatcher/Dispatcher.js
+++ script/dispatcher/Dispatcher.js
@@ -9,97 +9,96 @@
 
 "use strict";
 
-function Dispatcher() {
-  this._listeners = {};
-  this._lastID = -1;
+class Dispatcher {
+  constructor() {
+    this._listeners = new Map();
+    this._lastID = -1;
 
-  this._pendingPayload = null;
-  this._dispatching = false;
-}
-
-Dispatcher.prototype.register = function(cb) {
-  var id = 'cb' + (++this._lastID);
-
-  // A listener is an object with a callback field plus bookkeeping fields
-  // to allow listeners (stores) to wait for one another.
-  this._listeners[id] = {
-    callback: cb,
-    pending: false,
-    handled: false
-  };
-
-  return id;
-};
-
-Dispatcher.prototype.unregister = function(id) {
-  if (!this._listeners[id]) {
-    throw new Error('Not an active listener: ' + id);
-  }
-  // FIXME: What if we delete the listener from within a dispatch? Will
-  // something bad happen?
-  delete this._listeners[id];
-};
-
-Dispatcher.prototype.dispatch = function(payload) {
-  if (this._dispatching) {
-    throw new Error('Cannot dispatch from within a dispatch');
+    this._pendingPayload = null;
+    this._dispatching = false;
   }
 
-  this._startDispatch(payload);
-  try {
-    for (var k in this._listeners) {
-      var listener = this._listeners[k];
-      if (!listener.pending) {
-        this._invoke(listener);
-      }
-    }
-  } finally {
-    this._endDispatch();
-  }
-};
+  register(cb) {
+    let id = ++this._lastID;
 
-Dispatcher.prototype.waitFor = function(ids) {
-  if (!this._dispatching) {
-    throw new Error('waitFor can only be called within a dispatch');
+    // A listener is an object with a callback field plus bookkeeping fields
+    // to allow listeners (stores) to wait for one another.
+    this._listeners.set(id, {
+      callback: cb,
+      pending: false,
+      handled: false,
+    });
+
+    return id;
   }
 
-  var _this = this;
-  ids.forEach(function(id) {
-    if (!_this._listeners[id]) {
+  unregister(id) {
+    if (!this._listeners.has(id)) {
       throw new Error('Not an active listener: ' + id);
     }
-    var listener = _this._listeners[id];
-
-    if (!listener.pending) {
-      this._invoke(listener);
-    } else if (!listener.handled) {
-      throw new Error(
-        'Circular dependency detected: ' + id + ' already pending'
-      );
-    }
-  });
-};
-
-Dispatcher.prototype._invoke = function(listener) {
-  listener.pending = true;
-  listener.callback(this._pendingPayload);
-  listener.handled = true;
-};
-
-Dispatcher.prototype._startDispatch = function(payload) {
-  this._pendingPayload = payload;
-  this._dispatching = true;
-
-  for (var k in this._listeners) {
-    var listener = this._listeners[k];
-    listener.pending = false;
-    listener.handled = false;
+    // FIXME: What if we delete the listener from within a dispatch? Will
+    // something bad happen?
+    this._listeners.delete(id);
   }
-};
 
-Dispatcher.prototype._endDispatch = function() {
-  this._pendingPayload = null;
-  this._dispatching = false;
-};
+  dispatch(payload) {
+    if (this._dispatching) {
+      throw new Error('Cannot dispatch from within a dispatch');
+    }
+
+    this._startDispatch(payload);
+    try {
+      for (let listener of this._listeners.values()) {
+        if (!listener.pending) {
+          this._invoke(listener);
+        }
+      }
+    } finally {
+      this._endDispatch();
+    }
+  }
+
+  waitFor(ids) {
+    if (!this._dispatching) {
+      throw new Error('waitFor can only be called within a dispatch');
+    }
+
+    ids.forEach((id) => {
+      if (!this._listeners.has(id)) {
+        throw new Error('Not an active listener: ' + id);
+      }
+      var listener = this._listeners.get(id);
+
+      if (!listener.pending) {
+        this._invoke(listener);
+      } else if (!listener.handled) {
+        throw new Error(
+          'Circular dependency detected: ' + id + ' already pending'
+        );
+      }
+    });
+  }
+
+  _invoke(listener) {
+    listener.pending = true;
+    listener.callback(this._pendingPayload);
+    listener.handled = true;
+  }
+
+  _startDispatch(payload) {
+    this._pendingPayload = payload;
+    this._dispatching = true;
+
+    for (let listener of this._listeners.values()) {
+      listener.pending = false;
+      listener.handled = false;
+    }
+  }
+
+  _endDispatch() {
+    this._pendingPayload = null;
+    this._dispatching = false;
+  }
+}
 
 module.exports = Dispatcher;
