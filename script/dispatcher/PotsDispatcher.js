1c3e04205fbf7535c952a800c1e637b484bbc82c
Scott Feeney <scott@oceanbase.org>
2016-01-31 02:22:49 -0800

    batched updates

--- script/dispatcher/PotsDispatcher.js
+++ script/dispatcher/PotsDispatcher.js
@@ -2,7 +2,18 @@
 // @flow
 
 import Dispatcher from './Dispatcher';
+import {unstable_batchedUpdates as batchedUpdates} from 'react-dom';
 
-let PotsDispatcher = new Dispatcher();
+class PotsDispatcher extends Dispatcher {
+  _dispatch: (payload: Object) => void;
 
-export default PotsDispatcher;
+  constructor() {
+    super();
+    this._dispatch = Dispatcher.prototype.dispatch.bind(this);
+  }
+  dispatch(payload: Object) {
+    batchedUpdates(this._dispatch, payload);
+  }
+}
+
+export default new PotsDispatcher();
