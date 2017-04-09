132feba23439ecbcca5a00c08041fe70f8ba59f2
Scott Feeney <scott@oceanbase.org>
2017-04-09 13:44:35 -0700

    add logger to Redux store
    untested - none of the components I can interact with fire new Redux
    actions yet

--- script/stores/store.js
+++ script/stores/store.js
@@ -4,17 +4,25 @@
 import albumCache from '../reducers/albumCache';
 import playStatus from '../reducers/playStatus';
 import {
+  applyMiddleware,
   combineReducers,
   createStore,
 } from 'redux';
 import search from '../reducers/search';
 import trackCache from '../reducers/trackCache';
 
-const store = createStore(combineReducers({
-  albumCache,
-  playStatus,
-  search,
-  trackCache,
-}));
+const logger = store => next => action => {
+  console.info(action.type, action);
+};
+
+const store = createStore(
+  combineReducers({
+    albumCache,
+    playStatus,
+    search,
+    trackCache,
+  }),
+  applyMiddleware(logger)
+);
 
 export default store;
